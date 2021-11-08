15a13a43406cb581fdae65fb38c612b1f95a04d9
Adrian Quiroz <racing1236@hotmail.com>
2021-11-08 00:50:24 -0500

    Cambio

--- App.java
+++ App.java
@@ -1,54 +1,76 @@
-package Actividad8;
+package Actividad10;
+import javax.swing.JOptionPane;
 import java.util.Scanner;
 
-
 public class App {
-    public static void main(String[] args) {
-
-        Scanner Skan = new Scanner(System.in);
-        Deck mazo = new Deck();
-        int op;
-
-        System.out.println("Deck de Cartas para Iniciar: " + mazo.getSize());
-
-
-
-
-        for (Card elemento : mazo.getDeck()) {
-            System.out.print(elemento + " / ");
-        }
-        System.out.println("\n Que desea hacer?  ");
-
-        do{
-        System.out.println("1)Mezlar las cartas 2)Mostrar sig carta 3)Carta aleatoria 4)Cinco Cartas 5)Terminar proceso");
-        op = Skan.nextInt();
-
-        switch(op){
-        case 1:    
-        System.out.println("\n");
-        mazo.suflle();
-        return;
-
-        case 2:
-        System.out.println("\n");
-        mazo.head();
-        return;
-
-        case 3:
-        System.out.println("\n");
-        mazo.pick();
-        return;
-
-
-
-        case 4:
-        System.out.println("\n");
-        mazo.hand();
-        return;
+    public static void main(String[] args) throws Exception {
+        // Arrays que contienen los valores asignables al ArrayList "Deck"
         
+        Scanner Line = new Scanner(System.in);
+        
+        DeckClase testDeck = new DeckClase();
+        
+        boolean isPlaying = true;
+        char choice = 'x';
+        
+        // Ciclo principal
+        
+        while(isPlaying){
+            
+            /* 
+            
+            El ArrayList comienza en secuencia por defecto, por lo que 
+            necesitamos barajearlo al principio antes que el juego empiece.
+            
+            */
+            
+            testDeck.StartShuffle();
+            
+            choice = JOptionPane.showInputDialog(
+                    "Opciones: \n"
+                    + "a) Shuffle   b) Draw First\n"
+                    + "c) Random    d) Draw Five\n"
+                    + "e) Exit\n"
+                    + "Cartas en el Deck: " + testDeck.getSize()).charAt(0);
+                
+            
+            switch(choice){
+                
+                case'a':
+                    testDeck.Shuffle();
+                    break;
+                case'b':
+                    testDeck.HeadCard();
+                    break;
+                case'c':
+                    testDeck.PickRandom();
+                    break;
+                case'd':  
+                    testDeck.HandCard();
+                    break;
+                case'e':
+                    isPlaying = false;
+                    JOptionPane.showMessageDialog(null, "¡Gracias por jugar!");
+                    break;
+                    
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no reconocida.");
+                    throw new Exception("RECUERDE: Las letras tienen que estar en minusculas");
+            } 
+            
 
+
+
+            if(testDeck.getSize() == 0){
+                
+                isPlaying = false;
+                
+                JOptionPane.showMessageDialog(null, "El Deck está vacío. \n" + 
+                "¡Gracias por jugar!");
+                
             }
-        }while(op == 5);
+            
+        }
 
     }
 }
